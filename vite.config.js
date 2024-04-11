61719ca1f575646dc577737a09bf95ae95ab08cf
aescribano <aescribano@cendrassos.net>
2024-04-11 17:00:57 +0200

    Vitest Install

--- vite.config.js
+++ vite.config.js
@@ -17,4 +17,10 @@ export default defineConfig({
             },
         }),
     ],
+    test: {
+        globals: true,
+        environmentMatchGlobs:[
+            ['tests/JsTests/AjaxJsTest/**','node'],['tests/JsTests/domTests/**','jsdom']
+        ],   
+      },
 });
